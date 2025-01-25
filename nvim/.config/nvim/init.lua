37d74e65703ee8d8a2a33aa801e21d01afafbaf0
darbster145 <swoopmaster22@proton.me>
2025-01-25 16:53:42 -0700

    hypr stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -238,6 +238,13 @@ require("lazy").setup({
 	-- Use `opts = {}` to force a plugin to be loaded.
 	--
 
+  "windwp/nvim-ts-autotag",
+  opts = {
+    enable_close = true,
+    enable_rename = true,
+    enable_close_on_slash = true,
+  },
+  
 	-- Here is a more advanced example where we pass configuration
 	-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
 	--    require('gitsigns').setup({ ... })
@@ -792,13 +799,13 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"catppuccin/nvim",
+		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("catppuccin-mocha")
+			vim.cmd.colorscheme("tokyonight-night")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
@@ -852,8 +859,15 @@ require("lazy").setup({
 	},
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "windwp/nvim-ts-autotag" },
+    },
 		build = ":TSUpdate",
 		opts = {
+      highlight = { enable = true },
+      autopairs = { enable = true },
+      autotag = { enable = true },
+      indent = { enable = true },
 			ensure_installed = {
 				"bash",
 				"c",
